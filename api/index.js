4d3c324b8939fc6a29dfc0818660f4427df786ee
rmayer <cassettes90@gmail.com>
2020-02-05 03:38:02 -0300

    docs, first iteration

--- api/index.js
+++ api/index.js
@@ -4,6 +4,11 @@ const config = require('../config.js');
 const user = require('./components/user/network');
 const article = require('./components/article/network');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
+
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // ROUER
 app.use('/api/user', user);
 app.use('/api/article', article);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('API Listen on port 3000 ', config.api.port);
